c775c0a2602ecb12ed664a26f23a0d4658aac793
Arda Altun <141151283+Pacees@users.noreply.github.com>
2026-08-05 11:33:46 +0300

    chore(utils): add escapeHtml helper

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function escapeHtml(str: string) {
+  return String(str || '')
+    .replaceAll('&', '&amp;')
+    .replaceAll('<', '&lt;')
+    .replaceAll('>', '&gt;')
+    .replaceAll('"', '&quot;')
+    .replaceAll("'", '&#39;')
+}
